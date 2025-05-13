34b66faf032e3038c7f3270a9155a91ac42dbe83
sparrowstuff <damnagett@gmail.com>
2025-05-13 14:34:19 +0300

    header done, uislider problem

--- src/core/ui/layout/index.js
+++ src/core/ui/layout/index.js
@@ -1,21 +1,33 @@
-(function (window) {
-  const { config, lib } = window.app;
+;(function (window) {
+	const { config, lib } = window.app
 
-  const build = (element) => {
-    console.log(element);
-  };
+	const build = element => {
+		console.log(element)
+	}
 
-  const mount = () => {
-    const elements = Array.from(document.querySelectorAll('.selector'));
+	const mount = () => {
+		const elements = Array.from(document.querySelectorAll('.selector'))
 
-    elements.forEach(build);
+		elements.forEach(build)
 
-    // если нужен обсервер
-    // elements.forEach((element) => {
-    //   build(element);
-    //   lib.setObserver(element, build);
-    // });
-  };
+		// если нужен обсервер
+		// elements.forEach((element) => {
+		//   build(element);
+		//   lib.setObserver(element, build);
+		// });
+	}
 
-  document.addEventListener('DOMContentLoaded', mount);
-})(window);
+	document.addEventListener('DOMContentLoaded', mount)
+})(window)
+
+import 'nouislider/dist/nouislider.min.css'
+
+let slider = document.getElementById('slider')
+
+noUiSlider.create(slider, {
+	start: [20],
+	range: {
+		min: [0],
+		max: [100],
+	},
+})
